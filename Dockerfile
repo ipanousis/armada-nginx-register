eb086c3c71a528071606457133aac104e35d28b4
Yannis <ipanousis156@gmail.com>
2014-12-18 10:00:20 +0000

    externalise cluster domain

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,9 @@ MAINTAINER Yannis Panousis ipanousis156@gmail.com
 
 RUN apt-get -y install nginx
 
-ADD . /
-
 ENV NOTIFY python /nginx-register.py ; /etc/init.d/nginx configtest && /etc/init.d/nginx reload
 ENV INTERVAL 30
 
 EXPOSE 8080
+
+ADD . /
--- nginx-register.py
+++ nginx-register.py
@@ -4,7 +4,7 @@ import json, os, shutil
 import logging
 import re
 
-CLUSTER_NAME=os.environ['FLOCKER_DOMAIN']
+CLUSTER_DOMAIN=os.environ['CLUSTER_DOMAIN']
 
 containers = json.loads(open('/tmp/containers.json','r').read())['containers']
 
@@ -15,10 +15,10 @@ shutil.rmtree('/etc/nginx/https-redirects/*')
 def write_redirect_rule(subdomain, external_port):
   redirect_template = """
 if ($host ~ ^%s.*$) {
-  rewrite ^ https://flocker-1.ciscocloudsecurity.com:%s$request_uri permanent;
+  rewrite ^ https://%s:%s$request_uri permanent;
 }
 """
-  open('/etc/nginx/https-redirects/%s.conf' % subdomain).write(redirect_template % (subdomain, external_port))
+  open('/etc/nginx/https-redirects/%s.conf' % subdomain).write(redirect_template % (subdomain, CLUSTER_DOMAIN, external_port))
  
 for container in containers:
   for port in container['ports']:
